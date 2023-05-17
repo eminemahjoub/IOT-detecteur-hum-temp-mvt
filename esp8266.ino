bfe6ba71b1899f4d90b709e425d0088b19ebaafc
Amine Mahjoub <31483078+eminemahjoub@users.noreply.github.com>
2023-05-17 11:06:05 +0100

    Add files via upload

---
+++ esp8266.ino
@@ -0,0 +1,166 @@
+
+ 
+
+
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include "DHT.h"
+
+// Uncomment one of the lines bellow for whatever DHT sensor type you're using!
+#define DHTTYPE DHT11   // DHT 11
+//#define DHTTYPE DHT21   // DHT 21 (AM2301)
+//#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+
+// Change the credentials below, so your ESP8266 connects to your router
+const char* ssid = "smart";
+const char* password = "1122334455";
+
+
+// Change the variable to your Raspberry Pi IP address, so it connects to your MQTT broker
+const char* mqtt_server = "broker.emqx.io";
+//For example
+//const char* mqtt_server = "192.168.1.106";
+
+// Initializes the espClient. You should change the espClient name if you have multiple ESPs running in your home automation system
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+// DHT Sensor - GPIO 5 = D1 on ESP-12E NodeMCU board
+const int DHTPin = 5;
+
+// Lamp - LED - GPIO 4 = D2 on ESP-12E NodeMCU board
+
+
+int sensor = 13;
+
+// Initialize DHT sensor.
+DHT dht(DHTPin, DHTTYPE);
+
+// Timers auxiliar variables
+long now = millis();
+long lastMeasure = 0;
+
+
+
+// This functions connects your ESP8266 to your router
+void setup_wifi() {
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("WiFi connected - ESP IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+// This function is executed when some device publishes a message to a topic that your ESP8266 is subscribed to
+// Change the function below to add logic to your program, so when a device publishes a message to a topic that 
+// your ESP8266 is subscribed you can actually do something
+void callback(String topic, byte* message, unsigned int length) {
+  Serial.print("Message arrived on topic: ");
+  Serial.print(topic);
+  Serial.print(". Message: ");
+  String messageTemp;
+  
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)message[i]);
+    messageTemp += (char)message[i];
+  }
+  Serial.println();
+
+
+  Serial.println();
+}
+
+// This functions reconnects your ESP8266 to your MQTT broker
+// Change the function below if you want to subscribe to more topics with your ESP8266 
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    
+    if (client.connect("amine11222334455")) {
+      Serial.println("connected");  
+      // Subscribe or resubscribe to a topic
+      // You can subscribe to more topics (to control more LEDs in this example)
+      client.subscribe("amine/room/test");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+  
+  dht.begin();
+  pinMode(sensor, INPUT);
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+
+}
+
+int lastStatus = 0;
+// For this project, you don't need to change anything in the loop function. Basically it ensures that you ESP is connected to your broker
+void loop() {
+
+  if (!client.connected()) {
+    reconnect();
+  }
+  if(!client.loop())
+    client.connect("amine11222334455");
+
+  now = millis();
+  // Publishes new temperature and humidity every 30 seconds
+
+  if (now - lastMeasure > 30000) {
+    lastMeasure = now;
+    // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+    float humidity = dht.readHumidity();
+    // Read temperature as Celsius (the default)
+    float temperatureC = dht.readTemperature();
+    // Read temperature as Fahrenheit (isFahrenheit = true)
+    float temperatureF = dht.readTemperature(true);
+
+    // Check if any reads failed and exit early (to try again).
+    if (isnan(humidity) || isnan(temperatureC) || isnan(temperatureF)) {
+      Serial.println("Failed to read from DHT sensor!");
+      return;
+    }
+
+    // Publishes Temperature and Humidity values
+String sm = "{\"tmp\":"+String(temperatureC)+",\"hum\":"+String(humidity)+"}";
+    client.publish("amine/room/test", sm.c_str());
+    // client.publish("amine/room/test", String(humidity).c_str());
+    //Uncomment to publish temperature in F degrees
+    //client.publish("room/temperature", String(temperatureF).c_str());
+    
+    Serial.print("Humidity: ");
+    Serial.print(humidity);
+    Serial.println(" %");
+    Serial.print("Temperature: ");
+    Serial.print(temperatureC);
+    Serial.println(" ºC");
+    Serial.print(temperatureF);
+    Serial.println(" ºF");
+  }
+
+
+  int state = digitalRead(sensor);
+  if(state != lastStatus){
+    lastStatus = state;
+    client.publish("amine/room/pir", String(state).c_str());
+  }
+  delay(1000);
+} 
